3784d1bb86d4372f66b2046a7042dacea0ff1b95
Elizabeth <ehernandez@alumnos.uaq.mx>
2025-03-22 17:36:39 -0600

    se realizaron cambios

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const express = require ('express');
 const app = express();
@@ -10,12 +9,14 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get ("/", (req, res, next) => {
-   return res.status(200).send("Bienvenido al Pokedex");
+   return res.status(200).json({ code: 1, message: "Bienvenido al Pokedex"});
 });
 
 app.use("/pokemon", pokemon);
 
-
+app.use((req, res, next) => {
+    return res.status(404).json({ code:404, message: "URL no encontrada"});
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
